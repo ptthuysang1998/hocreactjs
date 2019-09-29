2ce56ba51408128b8934e9473a00cb763c3cd2e7
ptthuysang1998 <thuysang1998@gmail.com>
2019-09-29 20:29:54 +0700

    ada

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>to start a course, u should access src/App.js change and save to reload</h3>
+      </div>
+    )
+  }
+}
